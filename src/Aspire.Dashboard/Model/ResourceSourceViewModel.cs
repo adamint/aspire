fe0e8055d28760606160e601c4ef861ddae1dc9e
Adam Ratzman <adamratzman@microsoft.com>
2025-01-29 15:29:22 -0500

    start to account for different dcp default arg options

--- src/Aspire.Dashboard/Model/ResourceSourceViewModel.cs
+++ src/Aspire.Dashboard/Model/ResourceSourceViewModel.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Text;
+
 namespace Aspire.Dashboard.Model;
 
 public class ResourceSourceViewModel(string value, string? contentAfterValue, string valueToVisualize, string tooltip)
@@ -12,21 +14,36 @@ public class ResourceSourceViewModel(string value, string? contentAfterValue, st
 
     internal static ResourceSourceViewModel? GetSourceViewModel(ResourceViewModel resource)
     {
-        var executablePath = resource.TryGetExecutablePath(out var path) ? path : null;
+        string? executablePath;
+        (List<string>? Arguments, string? ArgumentsString, string FullCommandLine)? commandLineInfo;
 
-        (string? ArgumentsString, string FullCommandLine)? commandLineInfo = null;
-
-        if (resource.TryGetExecutableArguments(out var arguments))
+        if (resource.TryGetExecutablePath(out var path))
         {
-            var argumentsString = arguments.IsDefaultOrEmpty ? null : string.Join(" ", arguments);
-            commandLineInfo = (ArgumentsString: argumentsString, $"{executablePath} {argumentsString}");
+            executablePath = path;
+            commandLineInfo = GetCommandLineInfo(resource, executablePath);
+        }
+        else
+        {
+            executablePath = null;
+            commandLineInfo = null;
         }
 
         // NOTE projects are also executables, so we have to check for projects first
         if (resource.IsProject() && resource.TryGetProjectPath(out var projectPath))
         {
-            if (commandLineInfo is { ArgumentsString: { } argumentsString, FullCommandLine: { } fullCommandLine })
+            if (commandLineInfo is { Arguments: { } arguments, ArgumentsString: { } argumentsString, FullCommandLine: { } fullCommandLine })
             {
+                // if the command line arguments start with DCP defaults, hide them
+                List<List<string>> dcpDefaultArgumentsOptions = ["run", "--no-build", "--project"];
+
+                if (arguments.Count > dcpDefaultArguments.Count && arguments.Take(dcpDefaultArguments.Count).SequenceEqual(dcpDefaultArguments))
+                {
+                    // skip the project path too
+                    var skipCount = dcpDefaultArguments.Count + 1;
+                    argumentsString = string.Join(' ', arguments.Skip(skipCount));
+                    arguments.RemoveRange(0, skipCount);
+                }
+
                 return new ResourceSourceViewModel(value: Path.GetFileName(projectPath), contentAfterValue: argumentsString, valueToVisualize: fullCommandLine, tooltip: fullCommandLine);
             }
 
@@ -50,5 +67,64 @@ public class ResourceSourceViewModel(string value, string? contentAfterValue, st
         }
 
         return null;
+    }
+
+    private static (List<string>? Arguments, string? ArgumentsString, string FullCommandLine)? GetCommandLineInfo(ResourceViewModel resource, string executablePath)
+    {
+        if (resource.TryGetExecutableArguments(out var arguments))
+        {
+            if (arguments.IsDefaultOrEmpty)
+            {
+                return (Arguments: null, ArgumentsString: null, FullCommandLine: executablePath);
+            }
+
+            var escapedArguments = arguments.Select(EscapeCommandLineArgument).ToList();
+            var argumentsString = string.Join(' ', escapedArguments);
+
+            return (Arguments: escapedArguments, argumentsString, $"{executablePath} {argumentsString}");
+        }
+
+        return null;
+
+        // This method doesn't account for all cases, but does the most common
+        static string EscapeCommandLineArgument(string argument)
+        {
+            if (string.IsNullOrEmpty(argument))
+            {
+                return "\"\"";
+            }
+
+            if (argument.Contains(' ') || argument.Contains('"') || argument.Contains('\\'))
+            {
+                var escapedArgument = new StringBuilder();
+                escapedArgument.Append('"');
+
+                for (int i = 0; i < argument.Length; i++)
+                {
+                    char c = argument[i];
+                    switch (c)
+                    {
+                        case '\\':
+                            // Escape backslashes
+                            escapedArgument.Append('\\');
+                            escapedArgument.Append('\\');
+                            break;
+                        case '"':
+                            // Escape quotes
+                            escapedArgument.Append('\\');
+                            escapedArgument.Append('"');
+                            break;
+                        default:
+                            escapedArgument.Append(c);
+                            break;
+                    }
+                }
+
+                escapedArgument.Append('"');
+                return escapedArgument.ToString();
+            }
+
+            return argument;
+        }
     }
 }
